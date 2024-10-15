58c51eb437f213e690453f5be07180a16b2d3366
leomuso <Pinosenx@gmail.com>
2024-10-15 18:04:07 +0200

    exercise 4.10

--- main.c
+++ main.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 
 int main() {
-    int num, i = 0;
+    int num, i /*= 0*/;
     int potenza = 1;
     printf("inserire il numero di potenze di 2 da visualizzare: ");
     scanf("%d", &num);
     while (num < 2 || num > 20){
         printf("inserire un numero valido tra 2 e 20: ");
         scanf("%d", &num);
-    }
+    }/*
     while (i < num){
         printf("2^%d = %d\n", i, potenza);
         potenza = potenza * 2;
         i++;
+    }*/
+    for (i = 0; i < num; i++) {
+        printf("2^%d = %d\n", i, potenza);
+        potenza = potenza * 2;
     }
     return 0;
 }
